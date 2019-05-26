6c2e17da9f79796ee9ef439ad0e2cd8b88682b67
ushiostarfish <ushiostarfish@gmail.com>
2019-05-26 19:27:08 +0900

    ランバートサンプラの分離

--- common/material.hpp
+++ common/material.hpp
@@ -6,6 +6,7 @@
 #include "peseudo_random.hpp"
 #include "orthonormal_basis.hpp"
 #include "assertion.hpp"
+#include "lambertian_sampler.hpp"
 
 namespace rt {
 	class BxDF;
@@ -48,35 +49,6 @@ namespace rt {
 
 		// pdf for wi
 		virtual float pdf(const glm::vec3 &wo, const glm::vec3 &sampled_wi, const ShadingPoint &shadingPoint) const = 0;
-	};
-
-	// p(w) = cosθ / π
-	class CosThetaProportionalSampler {
-	public:
-		static glm::vec3 sample(PeseudoRandom *random, const glm::vec3 &Ng) {
-			float a = random->uniform();
-			float b = random->uniform();
-			float r = std::sqrt(a);
-			float theta = b * glm::pi<float>() * 2.0f;
-
-			// uniform in xy circle, a = r * r
-			float x = r * cos(theta);
-			float y = r * sin(theta);
-			
-			// unproject to hemisphere
-			float z = std::sqrt(std::max(1.0f - a, 0.0f));
-			
-			// local to global
-			OrthonormalBasis<float> basis(Ng);
-			return basis.localToGlobal(glm::vec3(x, y, z));
-		}
-		static float pdf(const glm::vec3 &sampled_wi, const glm::vec3 &Ng) {
-			float cosTheta = glm::dot(sampled_wi, Ng);
-			if (cosTheta < 0.0f) {
-				return 0.0f;
-			}
-			return cosTheta * glm::one_over_pi<float>();
-		}
 	};
 
 	class LambertianBRDF : public BxDF {
